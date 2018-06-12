07ae13d03213d7d37b916607698f522dbee1d483
Sheila Wittman <szandora789@gmail.com>
2018-06-12 21:54:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,25 +4,26 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0
     @discount = discount
-    @items [0]
+    @items = []
   end
 
-  def add_item(item, price, quantity = 1)
+  def add_item(item, price, quantity=1)
     @price = price
     @total += price * quantity
     if quantity > 1
       counter = 0
       while counter < quantity
-        @items << item
-        counter += 1
+      @items << item
+      counter +=1
       end
+    else
       @items << item
     end
   end
 
   def apply_discount
     if @discount > 0
-      @total -= @total * @discount/100
+       @total -= @total * @discount/100
       "After the discount, the total comes to $#{@total}."
     else
       "There is no discount to apply."
